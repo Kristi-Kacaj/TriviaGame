223bb0a68d7ca3704858b50eaa5c47c52e5e51a1
Kristi Kacaj <kristik.web@gmail.com>
2020-05-02 22:03:12 -0400

    added function to timer and funtion to cycle through the questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //Array for the questions, choices, and answer
 const triviaQuestions = [
     {
         question: "What does Sheldon's mom call him?",
@@ -11,7 +12,7 @@ const triviaQuestions = [
         answer: "Mayim Bialik",
     },
     {
-       question: "?Where do Sheldon, Amy, Raj, Howard, and Leonard work?",
+       question: "Where do Sheldon, Amy, Raj, Howard, and Leonard work?",
        choices: ["Caltech", "UCLA", "USC", "Cal Poly"],
        answer: "Caltech",
     },
@@ -27,18 +28,51 @@ const triviaQuestions = [
     }
 ]
 
-let countdown = 15;
+//Set global variables
+let counter = 15;
 let currQuestion = 0;
 let score = 0;
 let lost = 0;
 let timer;
 
-// Displays the question and choices 
+//Cycles through all of the questions and ends the game after last question in the array
+function nextQuestion() {
+    const questionsOver = (triviaQuestions.length - 1) === currQuestion;
+    if (questionsOver) {
+        console.log('Game over!');
+    } else{
+    currQuestion++;
+    getQuestions();
+    }
+}
+
+//Stops timer at 0. Mover to next question when timer runs out.
+function timesUp() {
+    clearInterval(timer);
+    lost++;
+    nextQuestion();
+}
+
+//Starts a 15 second countdown
+function countDown() {
+    counter--;
+
+    $('#time').html('Timer: ' + counter);
+
+    if (counter ===0) {
+        timesUp();
+    }
+}
+
+// Displays the question and timer
 function getQuestions() {
+    counter = 15;
+    timer = setInterval(countDown, 1000);
+
     const question = triviaQuestions[currQuestion].question;
     const choices = triviaQuestions[currQuestion].choices;
 
-    $('#time').html('Timer: ' + countdown);
+    $('#time').html('Timer: ' + counter);
     $('#game').html(`
         <h4>${question}</h4>
         ${getChoices(choices)}
@@ -46,6 +80,7 @@ function getQuestions() {
     
 }
 
+//Displays the choices
 function getChoices(choices) {
     let result = '';
     for (let i = 0; i < choices.length; i++) {
